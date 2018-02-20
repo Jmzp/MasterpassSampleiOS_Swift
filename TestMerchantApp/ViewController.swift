e1ccdad54b193178397e037d2fc70671e65cd88d
Jorge Zapata <cvem8165@gmail.com>
2018-02-20 14:37:33 -0500

    Initial Commit

---
+++ TestMerchantApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMerchantApp
+//
+//  Created by Jorge Zapata on 20/02/18.
+//  Copyright © 2018 Jorge Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
